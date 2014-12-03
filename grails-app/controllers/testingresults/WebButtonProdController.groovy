86d77141338f89f5ad2f94a906273f7489f0fe2f
Brian Warner <bwarner@tffosx.ifn.private>
2014-12-02 16:41:24 -0800

    updated rows

--- grails-app/controllers/testingresults/WebButtonProdController.groovy
+++ grails-app/controllers/testingresults/WebButtonProdController.groovy
@@ -10,7 +10,7 @@ class WebButtonProdController {
     static allowedMethods = [save: "POST", update: "PUT", delete: "DELETE"]
 
     def index(Integer max) {
-        params.max = Math.min(max ?: 10, 100)
+        params.max = Math.min(max ?: 1000, 10000)
         respond WebButtonProd.list(params), model: [webButtonProdInstanceCount: WebButtonProd.count()]
     }
 
